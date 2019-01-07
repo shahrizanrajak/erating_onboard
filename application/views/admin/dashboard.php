30faf1e2763c223cb39c678fb0b4d19bf1bd4f0f
Shahrizan bin Md Rajak <shahrizan@mampu.gov.my>
2019-01-07 09:32:32 +0800

    Commit message

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -13,7 +13,7 @@
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini">+</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>e-</b>RATING</span>
+          <span class="logo-lg"><b>e-</b>RATING Openshift</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
